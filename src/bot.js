ae26221329a52509e04ca27974b71e84ddc60ab2
Tumen Bairov <bairovt@gmail.com>
2021-04-20 14:28:48 +0900

    bot helpText; bot mw use;

--- src/bot.js
+++ src/bot.js
@@ -4,37 +4,44 @@ import { findRoute } from './findRoute.js';
 
 const botToken = '1641333989:AAG1Qj7QiVLG8TPmjMVF8tnVE0C_ZUScWqQ';
 
-const bot = new Telegraf(botToken);
+const bot = new Telegraf(botToken); //todo^ use process.env.BOT_TOKEN
 
-const helpTxt = `Для начала установите себя в качестве водителя либо пассажира.
-Эту настройку можно изменить командами -
-Для водителя:
-    еду
-Для пассажира:
-    ищу
+const helpTxt = `- Этот бот создан для удобства поиска попутчиков.
+- Для начала установите себя в качестве водителя или пассажира, отправив сообщение:
+  для водителя:
+      возьму
+  для пассажира:
+      ищу
+Эту настройку можно изменить отправив соотв. сообщение.
 
-Показать объявления по направлению:
+Примеры:
+- Показать объявления по направлению между 2 населенными пунктами:
     Чита Улан-Удэ
     Борзя Чита
-Показать объявления в оба направления:
-    2 Чита Забайкальск`;
+- Показать объявления в оба направления:
+    Чита & УУ`;
 
 bot.start((ctx) => {
   // first time: updateType: my_chat_member;
-  console.log(JSON.stringify(ctx, null, 2));  
+  // console.log(JSON.stringify(ctx.update, null, 2));  
+  console.log(ctx.update.message.from.id);  
   ctx.reply(helpTxt)
 });
 
 bot.use(async (ctx, next) => {
-  console.log(JSON.stringify(ctx, null, 2));
+  //todo: set ctx.state.role
+  console.time(`Processing update ${ctx.update.update_id}`);
+  // console.log(JSON.stringify(ctx.update, null, 2));
   // await ctx.reply('message of updateType:\n' + ctx.updateType + '\nrecieved')
-  return next();
+  await next();
+  console.timeEnd(`Processing update ${ctx.update.update_id}`);
 })
 
 bot.help((ctx) => ctx.reply(helpTxt));
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 
-bot.on('message', async (ctx, next) => {
+
+bot.on('text', async (ctx, next) => {
   const msgText = ctx.update.message.text;
   const route = routeParser(msgText);
   if (route.length < 2) {    
@@ -46,11 +53,16 @@ bot.on('message', async (ctx, next) => {
   let resp = '';
   for (let rec of recs) {
     resp = rec.Time + ': ' + rec.Body;
-    await ctx.reply(resp);
+    await ctx.telegram.sendMessage(ctx.message.chat.id, resp); // ctx.reply();
   }
-  
+  return next();
 });
-// bot.on('edited_message', ctx => ctx.reply('inside on edited_message\n' + ctx.update.edited_message.text));      
+
+// bot.on('message', async (ctx) => {
+//   ctx.reply(`on message`);
+// });
+
+// bot.on('edited_message', ctx => ctx.reply('inside on edited_message\n' + ctx.update.edited_message.text));
 
 bot.launch();
 
