397c6c41475c713c447c325b4aaf0f5d374042b5
Tumen Bairov <bairovt@gmail.com>
2021-04-24 18:14:14 +0900

    case db.url depend on env

--- src/lib/arangodb.js
+++ src/lib/arangodb.js
@@ -5,12 +5,13 @@ import config from 'config';
 import { Database} from 'arangojs';
 
 const url = config.get('db.url');
+const localUrl = config.get('db.localhost');
 const databaseName = config.get('db.databaseName');
 const username = config.get('db.username');
 const password = config.get('db.password');
 
 const db = new Database({
-  url,
+  url: process.env.NODE_ENV === 'production' ? url : localUrl,
   databaseName,
   auth: { username, password},
 });
